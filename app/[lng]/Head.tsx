b8da314fccb696677d82a621399851700013362d
Guido Squillace <info@guidosquillace.it>
2023-11-28 22:02:57 +0100

    description

--- app/[lng]/Head.tsx
+++ app/[lng]/Head.tsx
@@ -5,7 +5,7 @@ export default function Head() {
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta
         name="description"
-        content="Visit https://www.datocms.com/marketplace/starters for more starters"
+        content="È il momento di partecipare, perché libertà è partecipazione e quindi partecipazione è libertà. Il momento di riappropriarci di un bene nostro, di una passione comune che ci fa gioire, urlare, piangere, esultare. Vogliamo e dobbiamo essere tanti, perché si sogna meglio, e vogliamo partecipare attivamente alle sorti del Siena Calcio. È il momento di dire la nostra."
       />
       <link rel="icon" href="/images/favicon.ico" />
     </>
